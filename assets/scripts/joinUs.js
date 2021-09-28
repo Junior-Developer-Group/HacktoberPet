53f08b030219e696006837f8dd2bffc1b0b36b3a
Matea Howard <bl0wfacee@gmail.com>
2021-09-28 19:35:54 +0100

    setting up index.html file

---
+++ assets/scripts/joinUs.js
@@ -0,0 +1,6 @@
+console.log("%cHello stranger! 👋\n\n\n\n\nLooking to improve your dev skills? \n\n\n\n\nLet's do it together 💪 \n\n\n\n\n 👇 Join our group 👇\n\n", "color:dimgrey; font-size: 40px; background-color: pink; border-radius: 10px;");
+console.log("%cDiscord Channel:\n\n\n\nhttps://discord.gg/58YEbSgSAc\n\n\n\n", "font-size: 20px");
+console.log("%cOur Website: \n\n\n\nhttps://junior-developer-group.com\n\n\n\n", "font-size: 20px");
+
+console.log("%cWe also need designers 😉", "font-size: 40px");
+console.log("%c\n\n\n\n\n- Likii & the team 💛", "font-size: 20px");
