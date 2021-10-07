7787f932c54a3dfdac847a7b5b718af4dd159def
simretB05 <happysimret@gmai.com>
2021-10-07 02:27:07 -0600

    simretB05

--- assets/scripts/joinUs.js
+++ assets/scripts/joinUs.js
@@ -4,3 +4,48 @@ console.log("%cOur Website: \n\n\n\nhttps://junior-developer-group.com\n\n\n\n",
 
 console.log("%cWe also need designers 😉", "font-size: 40px");
 console.log("%c\n\n\n\n\n- Likii & the team 💛", "font-size: 20px");
+
+// simretB05 javascript/ /
+const choImages = document.querySelector(".card-cho__container").children;
+const content = document.querySelector(".card-cho__text")
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const text = document.querySelectorAll(".card-cho__text")
+const tottalText = text.length;
+const totalImages = choImages.length;
+let index = 0;
+
+prev.addEventListener('click', (e) => {
+    // console.log(e)
+    e.preventDefault();
+    nextImage('prev');
+});
+next.addEventListener('click', (e) => {
+    // console.log(e)
+    e.preventDefault();
+    nextImage('next');
+});
+
+
+function nextImage(direction) {
+    console.log(direction);
+    if (direction == 'next') {
+        index++;
+        if (index == totalImages) {
+            index = 0;
+        }
+    } else {
+        if (index == 0) {
+            index = totalImages - 1;
+
+        } else {
+            index--;
+        }
+    }
+    for (let i = 0; i < choImages.length; i++) {
+        choImages[i].classList.remove('show');
+
+    }
+    choImages[index].classList.add('show');
+}
+// simret B05 javascript //
